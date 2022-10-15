89287baea942a09d4958db214b019ace790dd73b
manoj1995madushanka <madushankamanoj414@gmail.com>
2022-10-15 11:46:51 +0530

    add queries and mutations

--- graphqlendpoint/src/main/java/com/graphql/entity/Subject.java
+++ graphqlendpoint/src/main/java/com/graphql/entity/Subject.java
@@ -14,7 +14,7 @@ import javax.persistence.*;
 public class Subject {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.SEQUENCE)
 	@Column(name = "id")
 	private Long id;
 	
